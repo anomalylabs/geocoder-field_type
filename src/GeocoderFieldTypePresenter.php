4d3406cccaded733c700452aa0ff562485537253
Ryan Thompson <service@aiwebsystems.com>
2016-01-17 00:37:49 -0600

    Clear up a few things

--- src/GeocoderFieldTypePresenter.php
+++ src/GeocoderFieldTypePresenter.php
@@ -83,7 +83,46 @@ class GeocoderFieldTypePresenter extends FieldTypePresenter
 
         $data['markers'] = implode('|', [$size, $color, $label]);
 
-        $url = 'http://maps.googleapis.com/maps/api/staticmap?' . http_build_query($data);
+        $url = 'http://maps.googleapis.com/maps/api/staticmap?'
+            . http_build_query($data)
+            . '&key=' . $this->object->key();
+
+        return $this->image->make($url)->setExtension(array_get($options, 'format', 'png'))->image();
+    }
+
+    /**
+     * Return a static image map for the formatted location.
+     *
+     * @param array $options
+     * @return null|string
+     */
+    public function formattedImage(array $options = [])
+    {
+        if (!$this->object->getValue()) {
+            return null;
+        }
+
+        $data = [
+            'center'         => $this->formattedPosition(),
+            'scale'          => array_get($options, 'scale', 1),
+            'zoom'           => $this->object->config('zoom', 13),
+            'format'         => array_get($options, 'format', 'png'),
+            'maptype'        => array_get($options, 'maptype', 'roadmap'),
+            'visual_refresh' => array_get($options, 'visual_refresh', true),
+            'size'           => array_get($options, 'width', 150) . 'x' . array_get($options, 'height', 100)
+        ];
+
+        $marker = array_get($options, 'marker');
+
+        $color = 'color:0xfa5b4a';
+        $size  = 'size:' . array_get($marker, 'marker', 'small');
+        $label = 'label:' . array_get($marker, 'label', 'A') . '|' . $this->formattedPosition();
+
+        $data['markers'] = implode('|', [$size, $color, $label]);
+
+        $url = 'http://maps.googleapis.com/maps/api/staticmap?'
+            . http_build_query($data)
+            . '&key=' . $this->object->key();
 
         return $this->image->make($url)->setExtension(array_get($options, 'format', 'png'))->image();
     }
@@ -96,6 +135,16 @@ class GeocoderFieldTypePresenter extends FieldTypePresenter
     public function url()
     {
         return 'https://www.google.com/maps/place/' . $this->position() . '/';
+    }
+
+    /**
+     * Return the map URL for formatted location.
+     *
+     * @return string
+     */
+    public function formattedUrl()
+    {
+        return 'https://www.google.com/maps/place/' . $this->formattedPosition() . '/';
     }
 
     /**
@@ -129,41 +178,11 @@ class GeocoderFieldTypePresenter extends FieldTypePresenter
     }
 
     /**
-     * Return the marker latitude.
+     * Return the latitude.
      *
      * @return double
      */
     public function latitude()
-    {
-        return array_get($this->object->getValue(), 'marker_latitude');
-    }
-
-    /**
-     * Return the marker longitude.
-     *
-     * @return double
-     */
-    public function longitude()
-    {
-        return array_get($this->object->getValue(), 'marker_longitude');
-    }
-
-    /**
-     * Return the formatted position.
-     *
-     * @return string
-     */
-    public function formattedPosition()
-    {
-        return $this->latitude() . ',' . $this->longitude();
-    }
-
-    /**
-     * Return the formatted latitude.
-     *
-     * @return double
-     */
-    public function formattedLatitude()
     {
         return array_get($this->object->getValue(), 'latitude');
     }
@@ -173,8 +192,38 @@ class GeocoderFieldTypePresenter extends FieldTypePresenter
      *
      * @return double
      */
-    public function formattedLongitude()
+    public function longitude()
     {
         return array_get($this->object->getValue(), 'longitude');
+    }
+
+    /**
+     * Return the formatted position.
+     *
+     * @return string
+     */
+    public function formattedPosition()
+    {
+        return $this->formattedLatitude() . ',' . $this->formattedLongitude();
+    }
+
+    /**
+     * Return the formatted latitude.
+     *
+     * @return double
+     */
+    public function formattedLatitude()
+    {
+        return array_get($this->object->getValue(), 'formatted_latitude');
+    }
+
+    /**
+     * Return the longitude.
+     *
+     * @return double
+     */
+    public function formattedLongitude()
+    {
+        return array_get($this->object->getValue(), 'formatted_longitude');
     }
 }
