9d04c225f70ea80b98749b16538af92a33309a32
MarceauKa <marceau@casals.fr>
2016-03-02 16:20:19 +0100

    Add the French localization

---
+++ resources/lang/fr/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Géolocalisation',
+    'name'        => 'Type de champs Géolocalisation',
+    'description' => 'Type de champs proposant la géolocalisation via Google Maps.'
+];
