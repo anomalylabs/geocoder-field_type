6b88b2ee436c5533e3a1ba42d1ce0e9370625331
Dominique Netters <d.d.netters@gmail.com>
2017-03-20 13:01:51 +0100

    Added reverse geocoding presenters

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'cache_time' => [
+        'name'         => 'Cache time',
+        'instructions' => 'Specify the time in minutes for which api calls will be cached.',
+    ],
+];
