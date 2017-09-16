2764646b2f3867614a40e55ecfe20152c7e96017
Denis Efremov <efremov.a.denis@gmail.com>
2017-09-16 05:26:50 +0300

    Update config.php

--- resources/config/config.php
+++ resources/config/config.php
@@ -4,16 +4,18 @@ return [
     'zoom'   => [
         'type'   => 'anomaly.field_type.slider',
         'config' => [
-            'min' => 0,
-            'max' => 21,
+            'min'           => 0,
+            'max'           => 21,
+            'default_value' => 13,
         ],
     ],
     'height' => [
         'type'   => 'anomaly.field_type.integer',
         'config' => [
-            'max'  => 1000,
-            'min'  => 50,
-            'step' => 1,
+            'max'           => 1000,
+            'min'           => 50,
+            'step'          => 1,
+            'default_value' => 200,
         ],
     ],
 ];
