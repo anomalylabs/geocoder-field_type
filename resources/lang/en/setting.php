a889213ca745aeabf2aa119da9aa9901021fb60c
Ryan Thompson <ryan@pyrocms.com>
2018-12-05 21:40:15 -0600

    Lang

--- resources/lang/en/setting.php
+++ resources/lang/en/setting.php
@@ -3,6 +3,7 @@
 return [
     'api_key' => [
         'name'         => 'API Key',
-        'instructions' => '',
+        'instructions' => 'You can find this information within your app at <a href="https://console.developer.google.com/" target="_blank">Google</a>.',
+        'warning'      => 'The specified key must have Maps (JavaScript) and Geocoding APIs enabled for it.',
     ],
 ];
